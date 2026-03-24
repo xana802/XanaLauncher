0b974a1a6fa69a45ed412d8719d13f400db12214
xana802 <brice.alexandre51200@gmail.com>
2026-03-24 18:06:57 +0100

    ajout de nom

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -3,8 +3,9 @@ const { DistributionAPI } = require('helios-core/common')
 const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
-// exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
+// exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
 exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
+
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
